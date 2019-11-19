2bc1a709194d3d91762e73e7cb1e22e378e21b82
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-11-19 16:05:52 -0600

    Added structure and average function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Body {
+    x: f64,
+    y: f64,
+    z: f64,
+    mass: f64
+}
+
+fn average(a: f64, b:f64) -> f64 {
+    (a + b) / 2.0
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
